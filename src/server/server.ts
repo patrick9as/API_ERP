634763fac1637b90238d390b9574b76dbecde53f
patrick9as <patrick9as@gmail.com>
2024-09-29 23:49:41 -0300

    cors + express.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { routes } from '../routes/index';
+var cors = require('cors');
 
 const server = express();
 
+server.use(express.json());
+server.use(cors());
 server.use(routes);
+
 
 server.get('/', (req, res) => {
     res.status(200).send('API ERP');
