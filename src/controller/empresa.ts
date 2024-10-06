b0b680c7f77ef8196b169cd9c18814458f8141c1
patrick9as <patrick9as@gmail.com>
2024-10-06 13:44:01 -0300

    clareza no nome dos métodos

--- src/controller/empresa.ts
+++ src/controller/empresa.ts
@@ -1,20 +1,20 @@
 import express from 'express';
-import { GetAll, GetByID, GetByNome, Post } from '../service/empresa';
+import { CadastrarEmpresa, BuscarEmpresas, BuscarEmpresaPorID, BuscarEmpresasPorNome } from '../service/empresa';
 
 const ControllerEmpresa = express.Router();
 
 ControllerEmpresa.post('/empresa', async(req, res) => {
     try {
-        res.status(201).json({ id: await Post(req.body) });
+        res.status(201).json({ id: await CadastrarEmpresa(req.body) });
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao publicar empresas: ' + error });
+        res.status(400).send('Erro ao inserir empresa: ' + error);
     }
 });
 
 ControllerEmpresa.get('/empresa', async (req, res) => {
     try {
         const nome = req.query.nome as string | undefined;
-        const empresas = nome ? await GetByNome(nome) : await GetAll();
+        const empresas = nome ? await BuscarEmpresasPorNome(nome) : await BuscarEmpresas();
         res.status(200).json(empresas);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar empresas' });
@@ -23,7 +23,7 @@ ControllerEmpresa.get('/empresa', async (req, res) => {
 
 ControllerEmpresa.get('/empresa/:id', async(req, res) => {
     try {
-        const empresas = await GetByID(parseInt(req.params.id));
+        const empresas = await BuscarEmpresaPorID(parseInt(req.params.id));
         res.status(200).json(empresas);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar empresas' });
